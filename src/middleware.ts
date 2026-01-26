b77f9375c9e681133faa9599ade7cde560c03bb5
Christian Alexander Soriano Barrientos <christiansorianob@gmail.com>
2026-01-26 17:16:37 -0500

    fix: make middleware matcher explicit for api routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -72,12 +72,9 @@ export function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         */
+        '/api/:path*',
+        '/login',
+        '/portal/:path*',
         '/((?!_next/static|_next/image|favicon.ico).*)',
     ],
 }
