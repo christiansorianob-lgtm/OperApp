07f29d09337b9a964a06c232b6bffcc3aa3c6c0e
Christian Alexander Soriano Barrientos <christiansorianob@gmail.com>
2026-01-26 18:24:59 -0500

    feat: add standard hello probe to test API routing

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,6 +1,7 @@
-
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
-    return NextResponse.json({ message: "Hello from OperApp API!" }, { status: 200 });
+    return NextResponse.json({ message: "Hello World API is working" }, { status: 200 });
 }
